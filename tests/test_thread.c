a18b14865c60109e7811e5c48787f08c2ae4f0b2
Elias Tchastikov <chastland@mail.ru>
2026-08-21 18:36:18 +0300

    Fix rand_s and rand_r compatibility

--- tests/test_thread.c
+++ tests/test_thread.c
@@ -59,9 +59,9 @@ static tlsf_thread_t ts;
 #endif
 
 #if defined(_MSC_VER)
-#define TLSF_RAND rand_s
+#define TLSF_RAND(x) (rand_s((x)), (unsigned)*(x))
 #else
-#define TLSF_RAND rand_r
+#define TLSF_RAND(x) rand_r(x)
 #endif
 
 /* ------------------------------------------------------------------ */
