58a49fa85435b74e8d7768c18992aef92c7273fe
Elias Tchastikov <chastland@mail.ru>
2026-08-24 13:57:26 +0300

    tear apart windows and psapi for clang-format

--- tests/bench.c
+++ tests/bench.c
@@ -23,8 +23,9 @@
 #ifndef WIN32_LEAN_AND_MEAN
 #define WIN32_LEAN_AND_MEAN
 #endif
-#include <psapi.h>
 #include <windows.h>
+
+#include <psapi.h>
 #else
 #include <sys/resource.h>
 #include <unistd.h>
--- tests/wcet.c
+++ tests/wcet.c
@@ -44,8 +44,9 @@
 #ifndef WIN32_LEAN_AND_MEAN
 #define WIN32_LEAN_AND_MEAN
 #endif
-#include <psapi.h>
 #include <windows.h>
+
+#include <psapi.h>
 #endif
 
 #ifdef __APPLE__
